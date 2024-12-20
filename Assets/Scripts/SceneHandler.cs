3a2fb41d58c409be440690dc743b422b7b8d0d86
Emilie Gonin <emilie.yuu@gmail.com>
2024-12-20 13:23:23 +0100

    update to new unity version

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -1,4 +1,5 @@
 using NaughtyAttributes;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,27 +8,49 @@ public class SceneHandler : MonoBehaviour
     [HorizontalLine(color: EColor.Green)]
     [BoxGroup("Load Settings"), SerializeField, Scene] private string _sceneToLoad;
     [BoxGroup("Load Settings"), SerializeField] private bool _loadAsAdditive;
+    [BoxGroup("Load Settings"), SerializeField] private bool _instantLoad;
+    [BoxGroup("Load Settings"), SerializeField] private bool _checkIfUnique = false;
 
     [HorizontalLine(color: EColor.Red)]
     [BoxGroup("Unload Settings"), SerializeField] private bool _unloadCurrentScene = true;
     [BoxGroup("Unload Settings"), HideIf("_unloadCurrentScene"), SerializeField, Scene]
     private string _sceneToUnload;
+    [BoxGroup("Unload Settings"), SerializeField] private bool _popupTimeout = false;
+    [BoxGroup("Unload Settings"), ShowIf("_popupTimeout"), SerializeField]
+    private int _timeoutDuration = 3;
 
     private LoadSceneMode _loadSceneMode;
 
     private void Awake()
     {
         _loadSceneMode = _loadAsAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+        if (!_instantLoad) return;
+        Load();
     }
 
     public void Load()
     {
+        if (_checkIfUnique && IsSceneLoaded()) return;
         SceneManager.LoadScene(_sceneToLoad, _loadSceneMode);
+        if (_popupTimeout) StartCoroutine(Timeout());
     }
 
     public void Unload()
     {
         string scene = _unloadCurrentScene ? gameObject.scene.name : _sceneToUnload;
         SceneManager.UnloadSceneAsync(scene);
+    }
+
+    private IEnumerator Timeout()
+    {
+        yield return new WaitForSeconds(_timeoutDuration);
+        Unload();
+        yield return null;
+    }
+
+    public bool IsSceneLoaded()
+    {
+        Scene scene = SceneManager.GetSceneByName(_sceneToLoad);
+        return scene.isLoaded;
     }
 }
